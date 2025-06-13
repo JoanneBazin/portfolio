6e4aa40a76080e00a15524bff4d341476f4e398d
Joanne <joanne.bazin@gmail.com>
2025-06-13 14:50:40 +0200

    feat: integration top navbar (tablet navigation)

--- components/layout/navigation/NavigationWrapper.tsx
+++ components/layout/navigation/NavigationWrapper.tsx
@@ -1,9 +1,11 @@
 import { SideBar } from "./SideBar";
+import { TopNav } from "./TopNav";
 
 export const NavigationWrapper = () => {
   return (
     <>
       <SideBar />
+      <TopNav />
     </>
   );
 };
---
+++ components/layout/navigation/TopNav.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useNavigation } from "@/hooks/useNavigation";
+
+export const TopNav = () => {
+  const { activeLink, hideSidebar, LINKS } = useNavigation();
+
+  if (hideSidebar) return null;
+
+  return (
+    <nav className="hidden sm:flex lg:hidden fixed top left-0 top-0 w-screen p-8 z-40">
+      <ul className="flex items-center justify-between w-full px-8">
+        {LINKS.map((link, index) => (
+          <li
+            key={index}
+            className={`text-montserrat text-xl ${
+              activeLink === link.id
+                ? "text-accent font-bold"
+                : "hover:text-accent transition-colors duration-300"
+            }`}
+          >
+            <a href={`#${link.id}`}>{link.label}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
